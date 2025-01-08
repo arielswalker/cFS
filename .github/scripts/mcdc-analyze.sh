cb5068679af651b2ef8e2b6df2f08a8bef694254
arielswalker <166176736+arielswalker@users.noreply.github.com>
2025-01-07 18:07:03 -0600

    TEST modules=$(find "$BASE_DIR" -maxdepth 2 -type f

--- .github/scripts/mcdc-analyze.sh
+++ .github/scripts/mcdc-analyze.sh
@@ -8,7 +8,8 @@ if [ -n "$MODULES" ]; then
     modules="$MODULES"
     echo "Test modules provided: $modules"
 else
-    modules=$(find "$BASE_DIR" -maxdepth 1 -type f | sed -E "s|^$BASE_DIR/([^/]+)\..*|\1|")
+    # Search for modules in given base_dir and in its immediate subdirectories
+    modules=$(find "$BASE_DIR" -maxdepth 2 -type f | sed -E "s|^$BASE_DIR/([^/]+)\..*|\1|")
     echo "No test modules provided. Automatically finding modules under $BASE_DIR."
 
     echo "List of found modules:"
